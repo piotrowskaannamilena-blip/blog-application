82692ec0e0219da9a6e7957aebb0937d374948c7
AnnnaPiotrowska <piotrowskaannamilena@gmail.com>
2025-11-16 21:25:59 +0000

    updated variables for connection and deployment on render

--- config/connection.js
+++ config/connection.js
@@ -1,54 +1,28 @@
-// require("dotenv").config();
-
-// const {Sequelize} = require("sequelize");
-
-// //for render:
-// const isProduction = process.env.NODE_ENV === "production";
-// let sequelize;
-
-// if (isProduction) {
-//   // For production on Render
-//   sequelize = new Sequelize(
-//     process.env.DATABASE_URL || process.env.JAWSDB_URL,
-//     {
-//       dialect: process.env.JAWSDB_URL ? "mysql" : "postgres",
-//       dialectOptions: {
-//         ssl: {
-//           require: true,
-//           rejectUnauthorized: false,
-//         },
-//       },
-//       logging: false,
-//     }
-//   );
-// } else {
-//   // For production in local DB
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(
-//       process.env.DB_DATABASE,
-//       process.env.DB_USERNAME,
-//       process.env.DB_PASSWORD,
-//       {
-//         host: process.env.DB_HOST,
-//         dialect: process.env.DB_DIALECT,
-//         port: process.env.DB_PORT,
-//       }
-//     );
-//   }
-// module.exports = sequelize;
-
-
 require("dotenv").config();
 
-const Sequelize = require("sequelize");
+const {Sequelize} = require("sequelize");
 
+//for render:
+const isProduction = process.env.NODE_ENV === "production";
+let sequelize;
 
-if (process.env.DB_PASSWORD === "ChangeMe!") {
-  console.error("Please update the .env file with your database password.");
-  process.exit(1);
-}
-
+if (isProduction) {
+  // For production on Render
+  sequelize = new Sequelize(
+    process.env.DATABASE_URL || process.env.JAWSDB_URL,
+    {
+      dialect: process.env.JAWSDB_URL ? "mysql" : "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false,
+    }
+  );
+} else {
+  // For production in local DB
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
@@ -59,9 +33,35 @@ const sequelize = process.env.JAWSDB_URL
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
         port: process.env.DB_PORT,
-// Logging locally for debugging
-        logging: console.log,
       }
     );
-
+  }
 module.exports = sequelize;
+
+
+// require("dotenv").config();
+
+// const Sequelize = require("sequelize");
+
+
+// if (process.env.DB_PASSWORD === "ChangeMe!") {
+//   console.error("Please update the .env file with your database password.");
+//   process.exit(1);
+// }
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(
+//       process.env.DB_DATABASE,
+//       process.env.DB_USERNAME,
+//       process.env.DB_PASSWORD,
+//       {
+//         host: process.env.DB_HOST,
+//         dialect: process.env.DB_DIALECT,
+//         port: process.env.DB_PORT,
+// // Logging locally for debugging
+//         logging: console.log,
+//       }
+//     );
+
+// module.exports = sequelize;
